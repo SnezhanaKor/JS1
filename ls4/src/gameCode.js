cc382eb6d924ba8a5e4652d9033d01c0fa906203
snezhana <snezhanakorolyova@yandex.ru>
2021-02-09 18:16:11 +0300

    lesson4
    4.1.done
    4.2.in-proc.

---
+++ ls4/src/gameCode.js
@@ -0,0 +1,141 @@
+//После игры необходимо спросить номер вопроса. 
+//По номеру вопроса нужно вывести текст вопроса и текст выбранного ответа
+
+var event, ok;
+/*//создан массив для записи результатов хода
+var answers = [];*/
+
+
+do { //Выводим первый вопрос
+    ok = false;
+    event = +prompt(works.a00 + works.a1 + works.a2 + '-1 - Выход из игры');
+
+    if (event == -1) {
+        break;
+    } else {
+        ok = isAnswer(works.a0, event);
+    }
+} while (!ok);
+switch (event) {
+    case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
+        do {
+            ok = false;
+            event = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            } else {
+                ok = isAnswer(works.b0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    } else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    } else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
+        do {
+            ok = false;
+            event = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            } else {
+                ok = isAnswer(works.c0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    } else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    } else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case -1: // Первое действие
+        break;
+    default:
+        alert('Ошибка');
+}
+//Показать результат хода
+var history = prompt ("Спасибо за игру. " + "Введите номер хода чтобы узнать его результат (0 вывести всю историю)");
+//
+
+//------------------------------------------
+function isAnswer(q, event) {
+    if (isNaN(event) || !isFinite(event)) {
+        alert('Вы ввели недопустимый символ');
+        return false;
+    } else if (event < 1 || event > q) {
+        alert('Ваше число выходит из допустимого диапозона');
+        return false;
+    }
+    return true;
+
+}
+/*Домашнее заданиe
+//            Выводим результат хода
+if (history > 0){
+    history -= 1;
+    alert("Результат хода: " + answers[history]);
+    }
+//             Если выбран ноль выводим всю историю
+    else {
+      a = [];
+      b = 0;
+      for(i = 1;i < answers.length;i++){
+        b++;
+        c = "Ход " + i + " Результат: " + answers[b]+'\r\n';
+        a.push(c);
+      }
+      alert(a.join(''));
+  }
+  */
+ 
